dda58dc031c9e34fae527258f09b113ce9c25247
Rudis Muiznieks <rmuiznieks@nextgxdx.com>
2015-10-05 15:21:48 -0500

    fix(script): removed quotes from aws-cli command args

--- run.sh
+++ run.sh
@@ -27,10 +27,10 @@ then
     if [ -f $file ];
     then
       fname=$(basename $file)
-      debug "aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$file' $WERCKER_S3PUT_AWSCLI_OPTIONS"
-      sync_output=$(aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$file' $WERCKER_S3PUT_AWSCLI_OPTIONS)
+      debug "aws s3api put-object --bucket $WERCKER_S3PUT_AWSCLI_BUCKET --key $WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname --body $file $WERCKER_S3PUT_AWSCLI_OPTIONS"
+      sync_output=$(aws s3api put-object --bucket $WERCKER_S3PUT_AWSCLI_BUCKET --key $WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname --body $file $WERCKER_S3PUT_AWSCLI_OPTIONS)
       if [[ $? -ne 0 ]];then
-        warning $sync_output
+        debug $sync_output
         fail 'aws-cli failed';
       fi
     fi
@@ -39,10 +39,10 @@ elif [ -f $WERCKER_S3PUT_AWSCLI_FILE ] ;
 then
   cd $WERCKER_ROOT
   fname=$(basename $WERCKER_S3PUT_AWSCLI_FILE)
-  debug "aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$WERCKER_S3PUT_AWSCLI_FILE' $WERCKER_S3PUT_AWSCLI_OPTIONS"
-  sync_output=$(aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$WERCKER_S3PUT_AWSCLI_FILE' $WERCKER_S3PUT_AWSCLI_OPTIONS)
+  debug "aws s3api put-object --bucket $WERCKER_S3PUT_AWSCLI_BUCKET --key $WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname --body $WERCKER_S3PUT_AWSCLI_FILE $WERCKER_S3PUT_AWSCLI_OPTIONS"
+  sync_output=$(aws s3api put-object --bucket $WERCKER_S3PUT_AWSCLI_BUCKET --key $WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname --body $WERCKER_S3PUT_AWSCLI_FILE $WERCKER_S3PUT_AWSCLI_OPTIONS)
   if [[ $? -ne 0 ]];then
-    warning $sync_output
+    debug $sync_output
     fail 'aws-cli failed';
   fi
 else
