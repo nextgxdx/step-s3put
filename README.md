074aa3be1e4aa4141c71ab1eba5a0a37bc1df833
Rudis Muiznieks <rmuiznieks@nextgxdx.com>
2015-10-05 14:55:45 -0500

    fix(step): enabled actual put-object commands
    - changed from pattern-based matching to file or folder (since using the
      '*' character in werkcer.yml causes it to barf)
    - going based on fait that $WERCKER_ROOT will point where I think it
      will point since local testing of this stuff is basically impossible.

--- README.md
+++ README.md
@@ -8,20 +8,23 @@ View this step in the [wercker directory][wd]
 
 ## Options
 
-* `file` (required) The name or pattern of the file(s) to upload.
 * `bucket` (required) The name of the bucket to upload to.
+* `file` (optional) The name of the file or directory to upload.
+  * Relative file paths are relative to the build's `$WERCKER_OUTPUT_DIR`.
+  * If this points to a directory, all files in that directory will be uploaded.
+  * Defaults to the build's `$WERCKER_OUTPUT_DIR` directory.
 * `key_prefix` (optional) Prefix for the s3 object keys.
 * `options` (optional) Additional parameters for the AWSCLI.
 
 ## Example
 
     - s3put:
-        file:       *.tar.gz
+        file:       /pipeline/output/my-project.tar.gz
         bucket:     mybucket
         key_prefix: myproject/
         options:    --acl bucket-owner-full-control
 
 [Wercker]: http://wercker.com
 [Amazon S3]: http://aws.amazon.com/s3
-[wd]: https://app.wercker.com/
+[wd]: https://app.wercker.com/#applications/5612a5904010ec244a242c71/tab/details
 [aws-cli]: https://github.com/aws/aws-cli
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 cd $HOME
 if [ ! -n "$WERCKER_S3PUT_AWSCLI_FILE" ]
 then
-  fail 'missing or empty option file, please check wercker.yml'
+  WERCKER_S3PUT_AWSCLI_FILE=$WERCKER_ROOT
 fi
 
 if [ ! -n "$WERCKER_S3PUT_AWSCLI_BUCKET" ]
@@ -18,20 +18,36 @@ fi
 
 info 'starting s3 upload'
 
-cd /pipeline/output
-pwd
-ls -alh
 
 set +e
-for file in $WERCKER_S3PUT_AWSCLI_FILE ; do
-  fname=$(basename $file)
-  debug "aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$file' $WERCKER_S3PUT_AWSCLI_OPTIONS"
-  #sync_output=$(aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$file' --body '$file' $WERCKER_S3PUT_AWSCLI_OPTIONS)
-  #if [[ $? -ne 0 ]];then
-  #  warning $sync_output
-  #  fail 'aws-cli failed';
-  #fi
-done
+if [ -d $WERCKER_S3PUT_AWSCLI_FILE ] ;
+then
+  cd $WERCKER_S3PUT_AWSCLI_FILE
+  for file in $WERCKER_S3PUT_AWSCLI_FILE/* ; do
+    if [ -f $file ];
+    then
+      fname=$(basename $file)
+      debug "aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$file' $WERCKER_S3PUT_AWSCLI_OPTIONS"
+      sync_output=$(aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$file' $WERCKER_S3PUT_AWSCLI_OPTIONS)
+      if [[ $? -ne 0 ]];then
+        warning $sync_output
+        fail 'aws-cli failed';
+      fi
+    fi
+  done
+elif [ -f $WERCKER_S3PUT_AWSCLI_FILE ] ;
+then
+  cd $WERCKER_ROOT
+  fname=$(basename $WERCKER_S3PUT_AWSCLI_FILE)
+  debug "aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$WERCKER_S3PUT_AWSCLI_FILE' $WERCKER_S3PUT_AWSCLI_OPTIONS"
+  sync_output=$(aws s3api put-object --bucket '$WERCKER_S3PUT_AWSCLI_BUCKET' --key '$WERCKER_S3PUT_AWSCLI_KEY_PREFIX$fname' --body '$WERCKER_S3PUT_AWSCLI_FILE' $WERCKER_S3PUT_AWSCLI_OPTIONS)
+  if [[ $? -ne 0 ]];then
+    warning $sync_output
+    fail 'aws-cli failed';
+  fi
+else
+  fail "file or folder $WERCKER_S3PUT_AWSCLI_FILE not found"
+fi
 
 success 'finished s3 upload';
 set -e
